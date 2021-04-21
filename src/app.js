aadf0f393b2843499f10b04aa49ac52512183275
Simon Siefke <simon.siefke@gmail.com>
2021-04-21 22:38:46 +0200

    add custom 404 page

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  const date = new Date().toString()
-  res.set('Cache-Control', 'public, max-age=31536000')
+  // const date = new Date().toString()
+  // res.set('Cache-Control', 'public, max-age=31536000')
+  res.status(200)
   res.send('hello world')
+})
+
+app.use((req, res, next) => {
+  res.status(404)
+  res.send('not found')
 })
 
 export { app }
