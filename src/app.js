72ed6e8e18ec4fa3eb6e48458515c31fbf18b447
Simon Siefke <simon.siefke@gmail.com>
2021-04-21 23:03:08 +0200

    add caching for static assets

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 
+app.use('/static', (req, res, next) => {
+  res.setHeader('Cache-Control', 'public, max-age=31536000, immutable')
+  next()
+})
+
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use((req, res, next) => {
